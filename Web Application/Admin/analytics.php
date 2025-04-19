b6c0f8bdac1b4da5d8f9d0d82d3f646e9bbb8095
Kaavya Fernando <92942865+katyx@users.noreply.github.com>
2025-04-19 15:51:26 +0530

    Update analytics.php

--- Web Application/Admin/analytics.php
+++ Web Application/Admin/analytics.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>analytics - CareU</title>
+    <title>analytics - Claim Mate</title>
 
     <link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
@@ -133,7 +133,7 @@
                 <!-- Copyright -->
                 <div class="text-center p-4">
                     © 2023 
-                <a class="text-reset fw-bold" href="https://CareU.online/">CareU</a>
+                <a class="text-reset fw-bold" href="https://CareU.online/">ClaimMate</a>
                 - All Rights Reserved
                 </div>
                 <!-- Copyright -->
