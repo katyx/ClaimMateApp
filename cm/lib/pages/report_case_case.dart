80b406cea3b5706a005491582a23cf5c239912ed
Kaavya Fernando <92942865+katyx@users.noreply.github.com>
2025-05-07 09:01:41 +0530

    Create report_case_case.dart

---
+++ cm/lib/pages/report_case_case.dart
@@ -0,0 +1,360 @@
+import 'package:claim_mate/components/button.dart';
+import 'package:claim_mate/pages/contact_us.dart';
+import 'package:claim_mate/pages/report_case_driver.dart';
+import 'package:claim_mate/pages/submited.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class ReportCaseCase extends StatefulWidget {
+  const ReportCaseCase({super.key});
+
+  @override
+  State<ReportCaseCase> createState() => _ReportCaseCaseState();
+}
+
+class _ReportCaseCaseState extends State<ReportCaseCase> {
+  final TextEditingController _locationController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+  String _currentLocation = '';
+  final bool _disableTextFields = true;
+
+  @override
+  void dispose() {
+    _locationController.dispose();
+    _dateController.dispose();
+    _timeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+    _getDateAndTime();
+  }
+
+  void _getDateAndTime() {
+    DateTime currentDate = DateTime.now();
+    String formattedDate =
+        currentDate.toString(); // Customize the date format as needed
+    _dateController.text = formattedDate.split(' ')[0]; // Extract date
+    _timeController.text =
+        formattedDate.split(' ')[1].split('.')[0]; // Extract time
+  }
+
+  Future<void> _getLocation() async {
+    final position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+    setState(() {
+      _currentLocation =
+          'Latitude: ${position.latitude}, Longitude: ${position.longitude}';
+      _locationController.text = _currentLocation;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              "lib/images/hero-img-01.png",
+            ),
+            opacity: 0.1,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  AppBar(
+                    elevation: 2,
+                    backgroundColor: Colors.white,
+                    leading: IconButton(
+                      icon: const Icon(Icons.arrow_back, color: Colors.black),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ReportCaseDriver()),
+                        );
+                      },
+                    ),
+                    title: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Report Your Case',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),
+                        ),
+                        Text(
+                          ' Case Details',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      Row(
+                        children: [
+                          const Icon(Icons.person, color: Colors.black),
+                          PopupMenuButton(
+                            icon: const Icon(Icons.arrow_drop_down,
+                                color: Colors.black),
+                            itemBuilder: (BuildContext context) {
+                              return [
+                                const PopupMenuItem(
+                                  value: 'profile',
+                                  child: Text('Profile'),
+                                ),
+                                const PopupMenuItem(
+                                  value: 'logout',
+                                  child: Text('Logout'),
+                                ),
+                              ];
+                            },
+                            onSelected: (String value) {
+                              if (value == 'profile') {
+                                // Do something when the user selects "Profile"
+                              } else if (value == 'logout') {
+                                // Do something when the user selects "Logout"
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 25),
+                  Container(
+                    width: 340,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 15),
+
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: 'Date',
+                              hintStyle: const TextStyle(color: Colors.black12),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              filled: true,
+                              fillColor: Colors.black12,
+                            ),
+                            controller: _dateController,
+                            enabled: !_disableTextFields,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: 'Time',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              filled: true,
+                              fillColor: Colors.black12,
+                            ),
+                            controller: _timeController,
+                            enabled: !_disableTextFields,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            maxLines: null,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                              hintText: 'Location',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              filled: true,
+                              fillColor: Colors.black12,
+                            ),
+                            controller: _locationController,
+                            enabled: !_disableTextFields,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            maxLines: null,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                              hintText: 'Any Other Reasons',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            maxLines: null,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                              hintText: 'Cause of Damage',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            maxLines: null,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                              hintText: 'Extend Damage',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+
+                        Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            maxLines: null,
+                            keyboardType: TextInputType.multiline,
+                            decoration: InputDecoration(
+                              hintText: 'Third Party Details',
+                              hintStyle: const TextStyle(color: Colors.grey),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        //Next Button
+                        SizedBox(
+                          height: 50.0,
+                          width: double.infinity,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 95),
+                            child: MYButton(
+                              gsntext: 'Submit',
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: const Text('Confirmation'),
+                                      content: const Text(
+                                          'Are you sure you want to submit?'),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                            // User selected No
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: const Text('No'),
+                                        ),
+                                        TextButton(
+                                          onPressed: () {
+                                            // User selected Yes
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      const Submited()),
+                                            );
+                                          },
+                                          child: const Text('Yes'),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(height: 15),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ContactUs()),
+                      );
+                    },
+                    child: const Text.rich(
+                      TextSpan(
+                        text: 'Need any ',
+                        style: TextStyle(
+                          color: Colors.black54,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'help?',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
