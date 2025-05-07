58815e3dcfdc063be78eb9e5ec510c915a77825a
Kaavya Fernando <92942865+katyx@users.noreply.github.com>
2025-05-07 08:20:01 +0530

    Create user.dart

---
+++ cm/lib/apis/user.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class UserDetailsService {
+  static Future<Map<String, dynamic>> getUserDetails(
+      String token, String nic) async {
+    final response = await http.get(
+      Uri.parse('http://careu.live/api/user-profile.php?nic=$nic'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body) as Map<String, dynamic>;
+    } else {
+      throw Exception('Failed to fetch user details');
+    }
+  }
+}
