08a2d2c6b521e292ea65b409f36886f8596b9bed
Kaavya Fernando <92942865+katyx@users.noreply.github.com>
2025-05-07 09:02:24 +0530

    Create report_case_driver.dart

---
+++ cm/lib/pages/report_case_driver.dart
@@ -0,0 +1,339 @@
+import 'package:claim_mate/components/button.dart';
+import 'package:claim_mate/pages/report_case_case.dart';
+import 'package:claim_mate/pages/report_case_vehicle.dart';
+import 'package:flutter/material.dart';
+
+class ReportCaseDriver extends StatefulWidget {
+  const ReportCaseDriver({super.key});
+
+  @override
+  _ReportCaseDriverState createState() => _ReportCaseDriverState();
+}
+
+class _ReportCaseDriverState extends State<ReportCaseDriver> {
+  DateTime? _selectedDate;
+  bool _isChecked = false;
+
+  void _presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(const Duration(days: 365)),
+    ).then((pickedDate) {
+      if (pickedDate != null) {
+        setState(() {
+          _selectedDate = pickedDate;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              "lib/images/hero-img-01.png",
+            ),
+            opacity: 0.1,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  AppBar(
+                    elevation: 2,
+                    backgroundColor: Colors.white,
+                    leading: IconButton(
+                      icon: const Icon(Icons.arrow_back, color: Colors.black),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ReportCaseVehicle()),
+                        );
+                      },
+                    ),
+                    title: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Report Your Case',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),
+                        ),
+                        Text(
+                          ' Driver Details',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      Row(
+                        children: [
+                          const Icon(Icons.person, color: Colors.black),
+                          PopupMenuButton(
+                            icon: const Icon(Icons.arrow_drop_down,
+                                color: Colors.black),
+                            itemBuilder: (BuildContext context) {
+                              return [
+                                const PopupMenuItem(
+                                  value: 'profile',
+                                  child: Text('Profile'),
+                                ),
+                                const PopupMenuItem(
+                                  value: 'logout',
+                                  child: Text('Logout'),
+                                ),
+                              ];
+                            },
+                            onSelected: (String value) {
+                              if (value == 'profile') {
+                                // Do something when the user selects "Profile"
+                              } else if (value == 'logout') {
+                                // Do something when the user selects "Logout"
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 35),
+                  Container(
+                    width: 340,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        CheckboxListTile(
+                          controlAffinity: ListTileControlAffinity.leading,
+                          title: const Text('Same as the owner'),
+                          value: _isChecked,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              _isChecked = value ?? false;
+                            });
+                          },
+                        ),
+                        SizedBox(
+                          height: 50.0,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: TextFormField(
+                              enabled: !_isChecked,
+                              decoration: InputDecoration(
+                                hintText: 'NIC No.',
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        SizedBox(
+                          height: 50.0,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: TextFormField(
+                              enabled: !_isChecked,
+                              decoration: InputDecoration(
+                                hintText: 'License No.',
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        SizedBox(
+                          height: 50.0,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: TextFormField(
+                              enabled: !_isChecked,
+                              decoration: InputDecoration(
+                                hintText: 'Categories',
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        SizedBox(
+                          height: 50.0,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: Stack(
+                              children: [
+                                TextFormField(
+                                  enabled: !_isChecked,
+                                  decoration: InputDecoration(
+                                    hintText: 'Expire Date',
+                                    hintStyle:
+                                        const TextStyle(color: Colors.grey),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  readOnly: true,
+                                  controller: TextEditingController(
+                                    text: _selectedDate == null
+                                        ? ''
+                                        : '${_selectedDate?.day}/${_selectedDate?.month}/${_selectedDate?.year}',
+                                  ),
+                                ),
+                                Positioned(
+                                  right: 0,
+                                  top: 5,
+                                  bottom: 5,
+                                  child: IconButton(
+                                    icon: const Icon(Icons.calendar_today),
+                                    onPressed: _presentDatePicker,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        SizedBox(
+                          height: 50.0,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: TextFormField(
+                              enabled: !_isChecked,
+                              decoration: InputDecoration(
+                                hintText: 'Driver Name',
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        SizedBox(
+                          height: 50.0,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: TextFormField(
+                              enabled: !_isChecked,
+                              decoration: InputDecoration(
+                                hintText: 'Address',
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        SizedBox(
+                          height: 50.0,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            child: TextFormField(
+                              enabled: !_isChecked,
+                              decoration: InputDecoration(
+                                hintText: 'Contact No.',
+                                hintStyle: const TextStyle(color: Colors.grey),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+
+                        //Next Button
+                        SizedBox(
+                          height: 50.0,
+                          width: double.infinity,
+                          child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 95),
+                              child: MYButton(
+                                gsntext: 'Next',
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const ReportCaseCase()),
+                                  );
+                                },
+                              )),
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  TextButton(
+                    onPressed: () {
+                      print('Need Help!');
+                    },
+                    child: const Text.rich(
+                      TextSpan(
+                        text: 'Need any ',
+                        style: TextStyle(
+                          color: Colors.black54,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'help?',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
